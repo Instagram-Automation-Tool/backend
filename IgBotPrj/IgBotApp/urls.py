63c26bd4b275e18fddebe7271785118f8968b0df
Andrei Sava <andisavauserul@gmail.com>
2022-10-10 13:32:07 +0200

    Revert "Added url routing for like/"post-link""
    This reverts commit 8080a56bb2cb7fb80b379ead97a41d40fc99832b.

--- IgBotPrj/IgBotApp/urls.py
+++ IgBotPrj/IgBotApp/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 from . import views
 
@@ -17,5 +17,4 @@ urlpatterns = [
         "follow/<username>",
         views.requestFollowProfile,
     ),
-    re_path("like", views.requestLikePost),
 ]
