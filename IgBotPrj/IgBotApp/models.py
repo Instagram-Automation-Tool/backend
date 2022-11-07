ee7ca96311819b6fae21974fc2164909ebe40457
Andrei Sava <andisavauserul@gmail.com>
2022-11-07 12:24:14 +0100

    Edited instagramaccount model

--- IgBotPrj/IgBotApp/models.py
+++ IgBotPrj/IgBotApp/models.py
@@ -5,7 +5,10 @@ from django.db import models
 # Create your models here.
 class InstagramAccount(models.Model):
     expandiId = models.CharField(max_length=100)
-    username = models.CharField(max_length=31)
+    username = models.CharField(max_length=31, primary_key=True)
     password = models.CharField(max_length=120)
-    cookies = models.TextField()
+    cookies = models.TextField
     proxy = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
