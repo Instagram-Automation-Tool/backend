51932a286cb0cd9c41e62d0a94c1541cc5836435
Andrei Sava <andisavauserul@gmail.com>
2022-12-26 16:19:09 +0100

    Updated setup.bat file with ALL dependencies.

--- setup.bat
+++ setup.bat
@@ -4,7 +4,7 @@ pip install virtualenv
 py -m venv IgBotEnv
 cd "IgBotEnv/Scripts"
 call activate.bat
-pip install django django-cors-headers psycopg2 selenium requests djangorestframework-simplejwt
+pip install django asgiref async-generator attrs black certifi cffi charset-normalizer click colorama distlib django-cors-headers django-selenium djangorestframework djangorestframework-simplejwt filelock h11 idna mypy-extensions mysqlclient outcome pathspec platformdirs psycopg2 psycopg2-binary pycparser PyJWT PyMySQL PySocks python-dotenv pytz requests selenium six sniffio sortedcontainers sqlparse tomli tqdm trio trio-websocket typing_extensions tzdata urllib3 virtualenv webdriver-manager wsproto
 cd "../../IgBotPrj"
 python manage.py makemigrations
 python manage.py migrate --fake
