00bc70fce336b468c7893d9d474d5846fcb3e5a5
Mihail <lepadatu.mihail1@gmail.com>
2023-01-06 10:26:14 +0100

    prepare for deployment

--- IgBotPrj/IgBotPrj/settings.py
+++ IgBotPrj/IgBotPrj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-kpmj19+acjn88)@u=kxy1@4_v88e-%svlnvmiv()^_rz%kpcn@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
